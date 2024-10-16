c49407c2da2bf97d029d528871c70fc3f4f81ca6
VarzarMiruna <125951638+VarzarMiruna@users.noreply.github.com>
2024-10-16 09:03:27 +0300

    Add files via upload

---
+++ Tema2/GolfScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro; 
+using UnityEngine;
+
+public class GolfScoreManager : MonoBehaviour
+{
+    public int hitCount = 0; //pentru scor
+    public TextMeshPro scoreText; //TextMeshPro
+
+    
+    private void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.CompareTag("GolfBall")){
+            hitCount++;
+            UpdateScoreText();
+        }
+    }
+
+    void UpdateScoreText(){
+        if (scoreText != null){
+            scoreText.text = "Scor: " + hitCount;
+        }
+    }
+}
+
+
